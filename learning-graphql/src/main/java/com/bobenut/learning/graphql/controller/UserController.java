16a8db1215f36b8b9cf82ed1fb6b97c630599fbf
bobenut <kongxh_1981@aliyun.com>
2020-01-09 23:41:52 +0800

    doc demo

--- learning-graphql/src/main/java/com/bobenut/learning/graphql/controller/UserController.java
+++ learning-graphql/src/main/java/com/bobenut/learning/graphql/controller/UserController.java
@@ -12,12 +12,25 @@ import com.bobenut.learning.graphql.service.UserGraphQLService;
 
 import graphql.ExecutionResult;
 
+/**
+ * 描述：
+ * 用户数据访问应用服务
+ * @version v1
+ */
 @RequestMapping("/uaa/api/v1/users")
 @RestController
 public class UserController {
 	@Autowired
 	private UserGraphQLService userGraphQLService;
 	
+
+	/**
+	 * 描述：
+	 * 查询用户数据，可一次性所有用户数据，支持分页获取
+	 * <p>处理1：如果用户不存在，咋地咋地</p>
+	 * @param query graphql查询语句
+	 * @return 用户数据
+	 */
 	@PostMapping
 	public ResponseEntity<Object> getAllUsers(@RequestBody String query) {
 		ExecutionResult execute = userGraphQLService.getGraphQL().execute(query);
--- learning-graphql/src/main/java/com/bobenut/learning/graphql/service/UserGraphQLService.java
+++ learning-graphql/src/main/java/com/bobenut/learning/graphql/service/UserGraphQLService.java
@@ -57,4 +57,34 @@ public class UserGraphQLService {
     public GraphQL getGraphQL(){
         return graphQL;
     }
+    
+    /**
+     * <h1>通过组织id删除组织</h1>
+     * <br>
+	 * <p>按照规则组合客户ID，生成对应的数据库名称。</p>
+     * <h2>验证参数：</h2>
+     * <ul>
+     *   <li>组织不存在的返回失败信息。</li>
+     *   <li>当前组织的节点类型是固定的时候，返回失败信息。</li>
+     * </ul>
+	 * <br>
+     * <h2>删除：</h2>
+     * <ul>
+     *   <li>通过组织id删除组织表中对应组织的记录，逻辑删除。</li>
+     *   <li>删除组织后，需要判断父组织是否存在子组织，存在的话，将父组织的层级关系修改为末节点。</li>
+     *   <li>更新组织索引表中的数据。</li>
+     *   <li>检查是否包含自定义字段，如果包含，删除自定义数据表中的数据。</li>
+     * </ul>
+     * <br>
+     * <h3>修改历史：</h3>
+     * <ul>
+     *   <li>2019-12-01 小明</li>
+     *   <li>2019-12-31 小王</li>
+     * </ul>
+     * <br>
+     * @param id 组织id
+     */
+    public void deleteOrg(String id) {
+    	
+    }
 }
